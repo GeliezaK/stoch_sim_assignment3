defd408097123e359a74c2c4a6c563aa8d196205
GeliezaK <gelieza98@t-online.de>
2022-12-10 13:13:38 +0100

    Add TSP configurations

--- import_tsp_graph.py
+++ import_tsp_graph.py
@@ -1,5 +1,17 @@
 import networkx as nx
 
+def construct_coordinate_table(filepath):
+    """"""
+
+def get_distance(a, b):
+    """
+
+    :param a:
+    :param b:
+    :return:
+    """
 
 if __name__ == '__main__':
+    filepath = '/tsp_configurations/eil51.tsp.txt'
+    construct_coordinate_table(filepath)
     pass
